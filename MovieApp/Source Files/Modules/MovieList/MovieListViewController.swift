fcfca796d192274f84ebe8d61858db93d9b748b5
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2021-02-24 14:28:46 +0100

    add search controller

--- MovieApp/Source Files/Modules/MovieList/MovieListViewController.swift
+++ MovieApp/Source Files/Modules/MovieList/MovieListViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 final class MovieListViewController: BaseViewController<MovieListView, MovieListViewModel> {
 
+    // MARK: Properties
+
+    private let searchController = UISearchController(searchResultsController: nil)
+
+    // MARK: Functions
+
     /// - SeeAlso: BaseViewController.setupView
     override func setupView() {
         view.accessibilityIdentifier = "view/movieList"
+        // todo localizable
         navigationItem.title = "Movie List"
+
+        setupSearchController()
     }
 
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        // todo localizable
+        searchController.searchBar.placeholder = "Search movie"
+        navigationItem.searchController = searchController
+    }
+}
+
+extension MovieListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+    }
 }
