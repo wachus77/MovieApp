b6bd4b70c5e537a169af30b4eabadf4cf1137241
Tomasz Iwaszek <wachus77@gmail.com>
2021-02-26 19:52:53 +0100

    add request tests

---
+++ MovieAppTests/API/Requests/MovieDetailsRequestTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDetailsRequestsTests.swift
+//  MovieAppTests
+//
+//  Created by TIWASZEK on 26/02/2021.
+//
+
+@testable import MovieApp
+import XCTest
+
+final class MovieDetailsRequestTests: XCTestCase {
+    private var sut: MovieDetailsRequest!
+
+    override func setUp() {
+        super.setUp()
+        sut = MovieDetailsRequest(imdbID: "dda2")
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testRequest() {
+        XCTAssertEqual(sut.path, "")
+        XCTAssertEqual(sut.method, .get)
+        XCTAssertEqual(sut.apiKey, "b9bd48a6")
+        XCTAssertEqual(sut.imdbID, "dda2")
+        XCTAssertEqual(sut.queryItems, [URLQueryItem(name: "apikey", value: "b9bd48a6"), URLQueryItem(name: "i", value: "dda2")])
+    }
+
+}
---
+++ MovieAppTests/API/Requests/MovieSearchRequestTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieSearchRequestTests.swift
+//  MovieAppTests
+//
+//  Created by TIWASZEK on 26/02/2021.
+//
+
+@testable import MovieApp
+import XCTest
+
+final class MovieSearchRequestTests: XCTestCase {
+    private var sut: MovieSearchRequest!
+
+    override func setUp() {
+        super.setUp()
+        sut = MovieSearchRequest(search: "test search", page: 1)
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testRequest() {
+        XCTAssertEqual(sut.path, "")
+        XCTAssertEqual(sut.method, .get)
+        XCTAssertEqual(sut.apiKey, "b9bd48a6")
+        XCTAssertEqual(sut.type, "movie")
+        XCTAssertEqual(sut.search, "test search")
+        XCTAssertEqual(sut.page, 1)
+        XCTAssertEqual(sut.queryItems, [URLQueryItem(name: "apikey", value: "b9bd48a6"), URLQueryItem(name: "s", value: "test search"), URLQueryItem(name: "type", value: "movie"), URLQueryItem(name: "page", value: "1")])
+    }
+
+}
